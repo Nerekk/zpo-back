681bf6c7e64384fc303d5f74185d4f73c95beb9a
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-01-30 11:05:52 +0100

    Initial commit

---
+++ src/main/java/com/project/zpo/ZpoApplication.java
@@ -0,0 +1,13 @@
+package com.project.zpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpoApplication.class, args);
+	}
+
+}
