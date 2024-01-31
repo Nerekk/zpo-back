6a48d4f2edc07701c7401505715bba08314be8d8
zurson <matu.jurek@gmail.com>
2024-01-31 11:23:13 +0100

    'Group not found' message is no longer in StudentMessages but in GroupMessages class

--- src/main/java/com/project/zpo/Students/Utils/StudentMessages.java
+++ src/main/java/com/project/zpo/Students/Utils/StudentMessages.java
@@ -5,6 +5,5 @@ public class StudentMessages {
     public static final String STUDENT_NOT_FOUND_MESSAGE = "Student not found!";
     public static final String STUDENT_REMOVED_MESSAGE = "Student has been removed!";
     public static final String STUDENT_INSERTED_MESSAGE = "Student inserted!";
-    public static final String STUDENT_GROUP_NOT_FOUND_MESSAGE = "Group not found!";
 
 }
