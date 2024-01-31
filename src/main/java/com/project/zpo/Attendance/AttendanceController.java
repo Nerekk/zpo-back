84307b223cf2ba0bcb0dbac5b7ecbf57ead24b5a
zurson <matu.jurek@gmail.com>
2024-01-31 11:25:53 +0100

    Deletion of unused imports

--- src/main/java/com/project/zpo/Attendance/AttendanceController.java
+++ src/main/java/com/project/zpo/Attendance/AttendanceController.java
@@ -2,15 +2,11 @@ package com.project.zpo.Attendance;
 
 import com.project.zpo.Attendance.Requests.SetAttendanceRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.project.zpo.Students.Utils.StudentMessages.STUDENT_REMOVED_MESSAGE;
 
 @RestController
 @RequestMapping(path = "attendances")
