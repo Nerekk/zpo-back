32ac3bc74adb0a2cfce4ae33bbccadf868129223
zurson <matu.jurek@gmail.com>
2024-01-31 11:24:22 +0100

    Addition of setAttendance method (without setting Term) + REST mapping

--- src/main/java/com/project/zpo/Attendance/AttendanceController.java
+++ src/main/java/com/project/zpo/Attendance/AttendanceController.java
@@ -1,9 +1,16 @@
 package com.project.zpo.Attendance;
 
+import com.project.zpo.Attendance.Requests.SetAttendanceRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.project.zpo.Students.Utils.StudentMessages.STUDENT_REMOVED_MESSAGE;
 
 @RestController
 @RequestMapping(path = "attendances")
@@ -14,4 +21,10 @@ public class AttendanceController {
     public AttendanceController(AttendanceService attendanceService) {
         this.attendanceService = attendanceService;
     }
+
+    @PostMapping
+    public ResponseEntity<String> setStudentAttendance(@RequestBody SetAttendanceRequest setAttendanceRequest) {
+        return attendanceService.setAttendance(setAttendanceRequest);
+    }
+
 }
--- src/main/java/com/project/zpo/Attendance/AttendanceService.java
+++ src/main/java/com/project/zpo/Attendance/AttendanceService.java
@@ -1,8 +1,19 @@
 package com.project.zpo.Attendance;
 
-import com.project.zpo.Groups.GroupRepository;
+import com.project.zpo.Attendance.Requests.SetAttendanceRequest;
+import com.project.zpo.Students.Student;
+import com.project.zpo.Students.StudentService;
+import com.project.zpo.Term.Term;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.project.zpo.Attendance.Utils.AttendanceMessages.ATTENDANCE_DATA_CORRUPTED;
+import static com.project.zpo.Attendance.Utils.AttendanceMessages.ATTENDANCE_OK;
+import static com.project.zpo.Students.Utils.StudentMessages.STUDENT_NOT_FOUND_MESSAGE;
 
 @Service
 public class AttendanceService {
@@ -11,6 +22,34 @@ public class AttendanceService {
     @Autowired
     public AttendanceService(AttendanceRepository attendanceRepository) {
         this.attendanceRepository = attendanceRepository;
+    }
+
+
+    public ResponseEntity<String> setAttendance(SetAttendanceRequest setAttendanceRequest) {
+
+        if (setAttendanceRequest == null ||
+                setAttendanceRequest.getDate() == null ||
+                setAttendanceRequest.getStudentAlbum() == null ||
+                setAttendanceRequest.getAttendanceStatus() == null
+        )
+            return new ResponseEntity<>(ATTENDANCE_DATA_CORRUPTED, HttpStatus.BAD_REQUEST);
+
+        Optional<Student> studentOptional = StudentService.getStudent(setAttendanceRequest.getStudentAlbum());
+        if (studentOptional.isEmpty())
+            return new ResponseEntity<>(STUDENT_NOT_FOUND_MESSAGE, HttpStatus.NOT_FOUND);
+
+        Student student = studentOptional.get();
+
+        Term term = new Term();
+        term.setDate(setAttendanceRequest.getDate());
+
+        // todo: get term
+
+        Attendance attendance = new Attendance(student, term, setAttendanceRequest.getAttendanceStatus().toString());
+
+        attendanceRepository.save(attendance);
+
+        return new ResponseEntity<>(ATTENDANCE_OK, HttpStatus.OK);
     }
 
 
