a75ea785d9feef6c8cf7d0ff3e83c2c6cabf749c
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-02-06 17:22:18 +0100

    JavaDoc for enums

--- src/main/java/com/project/zpo/Enumerations/AttendanceStatus.java
+++ src/main/java/com/project/zpo/Enumerations/AttendanceStatus.java
@@ -1,5 +1,8 @@
 package com.project.zpo.Enumerations;
 
+/**
+ * Enum representing possible attendance statuses: PRESENT, LATE, ABSENT, and NOT_SET.
+ */
 public enum AttendanceStatus {
 
     PRESENT,
