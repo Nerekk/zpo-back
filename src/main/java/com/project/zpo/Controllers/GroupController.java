4f69d7a2b1c92a3d2eebc31f7f0f0a39585c3c2a
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-02-06 16:56:57 +0100

    JavaDoc for controllers

--- src/main/java/com/project/zpo/Controllers/GroupController.java
+++ src/main/java/com/project/zpo/Controllers/GroupController.java
@@ -3,38 +3,78 @@ package com.project.zpo.Controllers;
 import com.project.zpo.RequestsAndResponses.GroupResponse;
 import com.project.zpo.Services.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller class responsible for handling group-related HTTP requests.
+ * This controller is mapped to the "/groups" endpoint.
+ * <p>
+ * The controller provides endpoints for adding, deleting, and retrieving groups.
+ * It utilizes the {@link GroupService} layer to handle business logic related to group management.
+ */
 @RestController
 @RequestMapping(path = "groups")
 @CrossOrigin
 public class GroupController {
+    /**
+     * Service responsible for handling group-related business logic.
+     */
     private final GroupService groupService;
 
+    /**
+     * Automatic constructor for the GroupController class.
+     *
+     * @param groupService The GroupService instance to be used by the controller.
+     */
     @Autowired
     public GroupController(GroupService groupService) {
         this.groupService = groupService;
     }
 
+    /**
+     * Endpoint for adding a new group.
+     *
+     * @param name The name of the group to be added.
+     * @return ResponseEntity representing the HTTP response.
+     * @see ResponseEntity
+     */
     @PostMapping(path = "{name}")
     public ResponseEntity<String> addGroup(@PathVariable("name") String name) {
         return groupService.addGroup(name);
     }
 
+    /**
+     * Endpoint for deleting an existing group.
+     *
+     * @param id The ID of the group to be deleted.
+     * @return ResponseEntity representing the HTTP response.
+     * @see ResponseEntity
+     */
     @DeleteMapping(path = "{id}")
     public ResponseEntity<String> deleteGroup(@PathVariable("id") Long id) {
         return groupService.deleteGroup(id);
     }
 
+    /**
+     * Endpoint for retrieving information about a specific group.
+     *
+     * @param id The ID of the group to be retrieved.
+     * @return ResponseEntity containing information about the group.
+     * @see ResponseEntity
+     */
     @GetMapping(path = "{id}")
     public ResponseEntity<GroupResponse> getGroupRequest(@PathVariable("id") Long id) {
         return groupService.getGroupRequest(id);
     }
 
+    /**
+     * Endpoint for retrieving information about all groups.
+     *
+     * @return List of GroupResponse objects containing information about all groups.
+     */
     @GetMapping(path = "all")
     public List<GroupResponse> getAllGroups() {
         return groupService.getAllGroups();
--- src/main/java/com/project/zpo/Controllers/StudentController.java
+++ src/main/java/com/project/zpo/Controllers/StudentController.java
@@ -7,29 +7,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller class responsible for handling student-related HTTP requests.
+ * This controller is mapped to the "/students" endpoint.
+ * <p>
+ * The controller provides endpoints for adding, removing, and changing student groups.
+ * It utilizes the {@link StudentService} layer to handle business logic related to student management.
+ */
 @RestController
 @RequestMapping(path = "students")
 @CrossOrigin
 public class StudentController {
 
+    /**
+     * Service responsible for handling student-related business logic.
+     */
     private final StudentService studentService;
 
+    /**
+     * Automatic constructor for the StudentController class.
+     *
+     * @param studentService The StudentService instance to be used by the controller.
+     */
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
 
+    /**
+     * Endpoint for adding a new student.
+     *
+     * @param addStudentRequest The request object containing information about the student to be added.
+     * @return ResponseEntity representing the HTTP response.
+     * @see ResponseEntity
+     */
     @PostMapping()
     public ResponseEntity<String> addStudent(@RequestBody AddStudentRequest addStudentRequest) {
         return studentService.addStudent(addStudentRequest);
     }
 
+    /**
+     * Endpoint for removing an existing student.
+     *
+     * @param albumToRemove The album number of the student to be removed.
+     * @return ResponseEntity representing the HTTP response.
+     * @see ResponseEntity
+     */
     @DeleteMapping("{album}")
     public ResponseEntity<String> removeStudent(@PathVariable("album") Long albumToRemove) {
         return studentService.removeStudent(albumToRemove);
     }
 
+    /**
+     * Endpoint for changing the group of a student.
+     *
+     * @param changeRequest The request object containing information about the group change.
+     * @return ResponseEntity representing the HTTP response.
+     * @see ResponseEntity
+     */
     @PutMapping("group/change")
     public ResponseEntity<String> changeStudentGroup(@RequestBody GroupChangeRequest changeRequest) {
         return studentService.changeGroup(changeRequest);
