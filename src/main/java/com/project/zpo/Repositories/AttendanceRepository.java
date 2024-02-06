640aabed227c6cf17029260c609544da0dfe0e37
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-02-06 17:20:30 +0100

    JavaDoc for repositories

--- src/main/java/com/project/zpo/Repositories/AttendanceRepository.java
+++ src/main/java/com/project/zpo/Repositories/AttendanceRepository.java
@@ -8,12 +8,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.time.LocalDate;
-
+/**
+ * Repository interface for managing Attendance entities in the database.
+ * Extends JpaRepository providing basic CRUD operations for Attendance entities.
+ */
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Attendance> {
 
-
+    /**
+     * Custom query method to find attendance by term and student.
+     *
+     * @param term    The term for which attendance is being searched.
+     * @param student The student for whom attendance is being searched.
+     * @return The Attendance object representing the attendance of the specified student in the given term.
+     */
     @Query("SELECT a FROM Attendance a WHERE a.term = :term and a.student = :student")
     Attendance findByTerm(@PathVariable("term") Term term, @PathVariable("student") Student student);
 
--- src/main/java/com/project/zpo/Repositories/TermRepository.java
+++ src/main/java/com/project/zpo/Repositories/TermRepository.java
@@ -8,8 +8,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 
+/**
+ * Repository interface for managing Term entities in the database.
+ * Extends JpaRepository providing basic CRUD operations for Term entities.
+ */
 @Repository
 public interface TermRepository extends JpaRepository<Term, Long> {
+
+    /**
+     * Custom query method to find a term by its date.
+     *
+     * @param date The date of the term to search for.
+     * @return The Term object representing the term with the specified date.
+     */
     @Query("SELECT t FROM Term t WHERE t.date = :date")
     Term findByDate(@PathVariable("date")LocalDate date);
 }
